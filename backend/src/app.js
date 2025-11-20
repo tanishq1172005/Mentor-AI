f1bee4a083126e6edd265426ac50a49b624dcd32
tanishq1172005 <tanishq1172005@gmail.com>
2025-11-20 12:39:27 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,9 @@ import cors from 'cors'
 
 export const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:'https://mentor-ai-xi.vercel.app'
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
